nrwl/nx

feat(core): add bun package manager
Bun uses yarn lock for it's binary file. Running the binary will produce the content of a yarn lock file (v1)

packages/nx/src/utils/package-manager.ts
@@ -12,7 +12,7 @@ import { readNxJson } from '../config/configuration';
 
 const execAsync = promisify(exec);
 
-export type PackageManager = 'yarn' | 'pnpm' | 'npm';
+export type PackageManager = 'yarn' | 'pnpm' | 'npm' | 'bun';
 
 export interface PackageManagerCommands {
   preInstall?: string;
@@ -37,6 +37,8 @@ export function detectPackageManager(dir: string = ''): PackageManager {
       ? 'yarn'
       : existsSync(join(dir, 'pnpm-lock.yaml'))
       ? 'pnpm'
+      : existsSync(join(dir, 'bun.lockb'))
+      ? 'bun'
       : 'npm')
   );
 }
@@ -111,6 +113,18 @@ export function getPackageManagerCommand(
         exec: 'npx',
         run: (script: string, args: string) => `npm run ${script} -- ${args}`,
         list: 'npm ls',
+      };
+    },
+    bun: () => {
+      return {
+        install: 'bun install',
+        ciInstall: 'bun install --frozen-lockfile',
+        add: 'bun add',
+        addDev: 'bun add -D',
+        rm: 'bun remove',
+        exec: 'bunx',
+        run: (script: string, args: string) => `bun ${script} ${args}`,
+        list: 'bun pm ls',
       };
     },
   };
@@ -314,12 +328,16 @@ export async function packageRegistryView(
   args: string
 ): Promise<string> {
   let pm = detectPackageManager();
-  if (pm === 'yarn') {
+  if (pm === 'yarn' || pm === 'bun') {
     /**
      * yarn has `yarn info` but it behaves differently than (p)npm,
      * which makes it's usage unreliable
      *
      * @see https://github.com/nrwl/nx/pull/9667#discussion_r842553994
+     *
+     * Bun has a pm ls function but it only relates to it's lockfile
+     * and acts differently from all other package managers
+     * from Jarred"it probably would be bun pm view <package-name>"
      */
     pm = 'npm';
   }
@@ -334,13 +352,15 @@ export async function packageRegistryPack(
   version: string
 ): Promise<{ tarballPath: string }> {
   let pm = detectPackageManager();
-  if (pm === 'yarn') {
+  if (pm === 'yarn' || pm === 'bun') {
     /**
      * `(p)npm pack` will download a tarball of the specified version,
      * whereas `yarn` pack creates a tarball of the active workspace, so it
      * does not work for getting the content of a library.
      *
      * @see https://github.com/nrwl/nx/pull/9667#discussion_r842553994
+     *
+     * bun doesn't current support pack
      */
     pm = 'npm';
   }

Jordan-Hall/onx-core

packages/nx/src/utils/package-manager.ts
@@ -13,7 +13,7 @@ import { workspaceRoot } from './workspace-root';
 
 const execAsync = promisify(exec);
 
-export type PackageManager = 'yarn' | 'pnpm' | 'npm';
+export type PackageManager = 'yarn' | 'pnpm' | 'npm' | 'bun';
 
 export interface PackageManagerCommands {
   preInstall?: string;
@@ -37,7 +37,9 @@ export function detectPackageManager(dir: string = ''): PackageManager {
   const nxJson = readNxJson();
   return (
     nxJson.cli?.packageManager ??
-    (existsSync(join(dir, 'yarn.lock'))
+    (existsSync(join(dir, 'bun.lockb'))
+      ? 'bun'
+      : existsSync(join(dir, 'yarn.lock'))
       ? 'yarn'
       : existsSync(join(dir, 'pnpm-lock.yaml'))
       ? 'pnpm'
@@ -148,6 +150,20 @@ export function getPackageManagerCommand(
         getRegistryUrl: 'npm config get registry',
       };
     },
+    bun: () => {
+      return {
+        install: 'bun install',
+        ciInstall: 'bun install --no-cache',
+        updateLockFile: 'bun install --frozen-lockfile',
+        add: 'bun install',
+        addDev: 'bun install -D',
+        rm: 'bun rm',
+        exec: 'bun',
+        dlx: 'bunx',
+        run: (script: string, args: string) => `bun run ${script} -- ${args}`,
+        list: 'bun pm ls',
+      };
+    },
   };
 
   return commands[packageManager]();
@@ -236,7 +252,12 @@ export function copyPackageManagerConfigurationFiles(
   root: string,
   destination: string
 ) {
-  for (const packageManagerConfigFile of ['.npmrc', '.yarnrc', '.yarnrc.yml']) {
+  for (const packageManagerConfigFile of [
+    '.npmrc',
+    '.yarnrc',
+    '.yarnrc.yml',
+    'bunfig.toml',
+  ]) {
     // f is an absolute path, including the {workspaceRoot}.
     const f = findFileInPackageJsonDirectory(packageManagerConfigFile, root);
     if (f) {
@@ -259,6 +280,10 @@ export function copyPackageManagerConfigurationFiles(
             readFileIfExisting(f)
           );
           writeFileSync(destinationPath, updated);
+          break;
+        }
+        case 'bunfig.toml': {
+          copyFileSync(f, destinationPath);
           break;
         }
       }
@@ -359,12 +384,16 @@ export async function packageRegistryView(
   args: string
 ): Promise<string> {
   let pm = detectPackageManager();
-  if (pm === 'yarn') {
+  if (pm === 'yarn' || pm === 'bun') {
     /**
      * yarn has `yarn info` but it behaves differently than (p)npm,
      * which makes it's usage unreliable
      *
      * @see https://github.com/nrwl/nx/pull/9667#discussion_r842553994
+     *
+     * Bun has a pm ls function but it only relates to its lockfile
+     * and acts differently from all other package managers
+     * from Jarred: "it probably would be bun pm view <package-name>"
      */
     pm = 'npm';
   }
@@ -379,13 +408,15 @@ export async function packageRegistryPack(
   version: string
 ): Promise<{ tarballPath: string }> {
   let pm = detectPackageManager();
-  if (pm === 'yarn') {
+  if (pm === 'yarn' || pm === 'bun') {
     /**
      * `(p)npm pack` will download a tarball of the specified version,
      * whereas `yarn` pack creates a tarball of the active workspace, so it
      * does not work for getting the content of a library.
      *
      * @see https://github.com/nrwl/nx/pull/9667#discussion_r842553994
+     *
+     * bun doesn't currently support pack
      */
     pm = 'npm';
   }